0acd4986ca5ea1b932586537c9bcae944dd77f9f
Robert Sapp <46914468+robwsapp@users.noreply.github.com>
2024-05-07 08:35:43 -0400

    Add facebook pixel

--- src/App.js
+++ src/App.js
@@ -19,6 +19,24 @@ import mainLogo from'./lakeside-logo.png';
 
 import './App.css';
 
+const FacebookPixel = () => {
+  React.useEffect(() => {
+    // Facebook Pixel Code
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function()
+    {n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1316904239259395');
+    fbq('track', 'PageView');
+  }, []);
+
+  return null;
+};
+
 function App() {
   const [currentStep, setCurrentStep] = useState(0);
   const [hasDrawings, setHasDrawings] = useState(null);
